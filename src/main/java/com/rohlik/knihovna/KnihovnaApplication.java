20e20df8054457b7d78360aa0845a2952745392d
Jan Šplíchal <jan.splichal@Jans-MacBook-Pro.local>
2019-03-21 10:09:41 +0100

    init commit

---
+++ src/main/java/com/rohlik/knihovna/KnihovnaApplication.java
@@ -0,0 +1,13 @@
+package com.rohlik.knihovna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnihovnaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnihovnaApplication.class, args);
+    }
+
+}
